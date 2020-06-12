c96ee56affb86931cad0d736cad84b87febf9a35
Mrinmay Mukherjee <devmrin@gmail.com>
2020-06-12 22:10:33 +0530

    reduced scope of avatar

--- src/Avatar/StyledAvatar.js
+++ src/Avatar/StyledAvatar.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const Avatar = styled.img`
+  height: 200px;
+  width: 200px;
   border-radius: 9999px;
   box-sizing: border-box;
 `;
--- src/Avatar/index.js
+++ src/Avatar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import StyledAvatar from "./StyledAvatar";
 
-export default function Avatar({ props }) {
-  return <StyledAvatar {...props} />;
+export default function Avatar() {
+  return <StyledAvatar src="https://mrinmay.com/about.jpg" alt="mrinmay" />;
 }
